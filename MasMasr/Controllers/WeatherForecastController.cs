ed135c47d7f82598a95a89db48dba8ae66e5daec
Ahmed Hegazy <infinite.group11@outlook.com>
2022-06-26 18:24:35 +0200

    ServerFolderURL

--- MasMasr/Controllers/WeatherForecastController.cs
+++ MasMasr/Controllers/WeatherForecastController.cs
@@ -1,16 +1,16 @@
 ﻿using MasMasr.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace MasMasr.Controllers
 {
-    [Authorize(Roles = UserRoles.Admin)]
+    //[Authorize(Roles = UserRoles.Admin)]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -20,16 +20,26 @@ namespace MasMasr.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private string _ServerFolderURL;
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+            GetServerFolder();
+        }
+
+        private void GetServerFolder()
+        {
+            _ServerFolderURL= _httpContextAccessor.HttpContext.Request.Scheme+"://"+_httpContextAccessor.HttpContext.Request.Host.Value+"/Upload/";
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var test = _ServerFolderURL;
             var identity = HttpContext.User.Identity as ClaimsIdentity;
             if (identity != null)
             {
